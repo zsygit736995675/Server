785c1bef1de2b5e3431e5cb07ba970f4375a6562
guangchengzi <guangchengzi@newborntown.com>
2020-12-04 10:05:12 +0800

    服务器基础

---
+++ MyServer/MyServer/MyHttpServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MyServer
+{
+    public class MyHttpServer : HttpServer
+    {
+        public MyHttpServer(int port): base(port)
+        {
+        }
+
+        public override void handleGETRequest(HttpProcessor p)
+        {
+            Console.WriteLine("request: {0}", p.http_url);
+            p.writeSuccess();
+            p.outputStream.WriteLine("成功啦:"+p.http_url);
+        }
+
+        public override void handlePOSTRequest(HttpProcessor p, StreamReader inputData)
+        {
+            Console.WriteLine("POST request: {0}", p.http_url);
+            string data = inputData.ReadToEnd();
+
+            p.outputStream.WriteLine("<html><body><h1>test server</h1>");
+            p.outputStream.WriteLine("<a href=/test>return</a><p>");
+            p.outputStream.WriteLine("postbody: <pre>{0}</pre>", data);
+        }
+    }
+}
---
+++ MyServer/MyServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MyServer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HttpServer httpServer = new MyHttpServer(2341);
+            Thread thread1 = new Thread(new ThreadStart(httpServer.listen));
+            thread1.Start();
+        }
+    }
+}
