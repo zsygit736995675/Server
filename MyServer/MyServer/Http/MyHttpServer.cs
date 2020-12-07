060f6d790c9e4ee90eccf5c604c421fb4d99323a
guangchengzi <guangchengzi@newborntown.com>
2020-12-07 18:02:26 +0800

    同步一下 添加了Mysql库

---
+++ MyServer/MyServer/Http/MyHttpServer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace MyServer
+{
+    public class MyHttpServer : HttpServer
+    {
+        public MyHttpServer(int port): base(port)
+        {
+
+            String connetStr = "server=127.0.0.1;port=3306;user=root;password=Sqltest1234; database=myschema;";
+            MySqlConnection conn = new MySqlConnection(connetStr);
+            try
+            {
+                conn.Open();//打开通道，建立连接，可能出现异常,使用try catch语句
+                Console.WriteLine("已经建立连接");
+                //在这里使用代码对数据库进行增删查改
+
+                string sql = "SELECT * FROM myschema.new_table;";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();//执行ExecuteReader()返回一个MySqlDataReader对象
+                while (reader.Read())//初始索引是-1，执行读取下一行数据，返回值是bool
+                {
+                    //Console.WriteLine(reader[0].ToString() + reader[1].ToString() + reader[2].ToString());
+                    //Console.WriteLine(reader.GetInt32(0)+reader.GetString(1)+reader.GetString(2));
+                    Console.WriteLine(reader.GetInt32("id") + reader.GetString("new_tablecol") ); 
+                }
+
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public override void handleGETRequest(HttpProcessor p)
+        {
+            Console.WriteLine("request: {0}", p.http_url);
+            p.writeSuccess();
+
+           
+
+          //  p.outputStream.WriteLine("helloworld:"+p.http_url);
+        }
+
+        public override void handlePOSTRequest(HttpProcessor p, StreamReader inputData)
+        {
+            Console.WriteLine("POST request: {0}", p.http_url);
+            string data = inputData.ReadToEnd();
+
+            p.outputStream.WriteLine("<html><body><h1>test server</h1>");
+            p.outputStream.WriteLine("<a href=/test>return</a><p>");
+            p.outputStream.WriteLine("postbody: <pre>{0}</pre>", data);
+        }
+    }
+}
