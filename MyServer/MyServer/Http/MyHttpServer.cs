51bf2f5781bbd668f24c7de91b2f324610eae737
guangchengzi <guangchengzi@newborntown.com>
2020-12-07 18:32:03 +0800

    同步

--- MyServer/MyServer/Http/MyHttpServer.cs
+++ MyServer/MyServer/Http/MyHttpServer.cs
@@ -4,48 +4,22 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using MySql.Data.MySqlClient;
+using MyServer.SQL;
 
 namespace MyServer
 {
     public class MyHttpServer : HttpServer
     {
+        MySqlMng sql;
+
         public MyHttpServer(int port): base(port)
         {
+            sql = new MySqlMng();
 
-            String connetStr = "server=127.0.0.1;port=3306;user=root;password=Sqltest1234; database=myschema;";
-            MySqlConnection conn = new MySqlConnection(connetStr);
-            try
+            MySqlDataReader reader = sql.getmysqlread("SELECT * FROM myschema.new_table;");
+            while (reader.Read())//初始索引是-1，执行读取下一行数据，返回值是bool
             {
-                conn.Open();//打开通道，建立连接，可能出现异常,使用try catch语句
-                Console.WriteLine("已经建立连接");
-                //在这里使用代码对数据库进行增删查改
-
-                string sql = "SELECT * FROM myschema.new_table;";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                MySqlDataReader reader = cmd.ExecuteReader();//执行ExecuteReader()返回一个MySqlDataReader对象
-                while (reader.Read())//初始索引是-1，执行读取下一行数据，返回值是bool
-                {
-                    //Console.WriteLine(reader[0].ToString() + reader[1].ToString() + reader[2].ToString());
-                    //Console.WriteLine(reader.GetInt32(0)+reader.GetString(1)+reader.GetString(2));
-                    Console.WriteLine(reader.GetInt32("id") + reader.GetString("new_tablecol") ); 
-                }
-
-            }
-            catch (MySqlException ex)
-            {
-                switch (ex.Number)
-                {
-                    case 0:
-                        Console.WriteLine("Cannot connect to server.  Contact administrator");
-                        break;
-                    case 1045:
-                        Console.WriteLine("Invalid username/password, please try again");
-                        break;
-                }
-            }
-            finally
-            {
-                conn.Close();
+                Console.WriteLine(reader.GetInt32("id") + reader.GetString("new_tablecol"));//"userid"是数据库对应的列名，推荐这种方式
             }
         }
 
@@ -54,7 +28,7 @@ namespace MyServer
             Console.WriteLine("request: {0}", p.http_url);
             p.writeSuccess();
 
-           
+
 
           //  p.outputStream.WriteLine("helloworld:"+p.http_url);
         }
---
+++ MyServer/MyServer/SQL/MySqlMng.cs
@@ -0,0 +1,71 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyServer.SQL
+{
+    class MySqlMng
+    {
+
+        /// <summary>
+        /// 建立数据库连接.
+        /// </summary>
+        public MySqlConnection getmysqlcon()
+        {
+            string M_str_sqlcon = "server=localhost; port=3306; user=root; password=Sqltest1234; database=myschema;"; //根据自己的设置
+            MySqlConnection conn = new MySqlConnection(M_str_sqlcon);
+            try
+            {
+                conn.Open();//打开通道，建立连接，可能出现异常,使用try catch语句
+                Console.WriteLine("数据库已经建立连接");
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+            }
+            return conn;
+        }
+
+        /// <summary>
+        /// 执行MySqlCommand
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        public void getmysqlcom(string M_str_sqlstr)
+        {
+            MySqlConnection mysqlcon = this.getmysqlcon();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            mysqlcom.ExecuteNonQuery();
+            mysqlcom.Dispose();
+            mysqlcon.Close();
+            mysqlcon.Dispose();
+        }
+
+        /// <summary>
+        /// 创建一个MySqlDataReader对象
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        /// <returns>返回MySqlDataReader对象</returns>
+        public MySqlDataReader getmysqlread(string M_str_sqlstr)
+        {
+            MySqlConnection mysqlcon = this.getmysqlcon();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            MySqlDataReader mysqlread = mysqlcom.ExecuteReader(CommandBehavior.CloseConnection);
+            return mysqlread;
+        }
+
+
+      
+    }
+}
